68533138e113a3dcebe440bea3ff5b9baa59a59d
myeonghan-nim <hannim0828@gmail.com>
2021-09-30 20:50:12 +0900

    Change: app name and todo logic

--- todo/forms.py
+++ todo/forms.py
@@ -1,16 +1,16 @@
 from django import forms
+
 from .models import Todo
 
 
 class TodoForm(forms.ModelForm):
-
     due_date = forms.DateField(
-                    widget=forms.DateInput(
-                        attrs={
-                            'type': 'date',
-                        }
-                    )
-                )
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+            }
+        )
+    )
 
     class Meta:
         model = Todo
