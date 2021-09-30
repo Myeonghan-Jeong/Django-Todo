8a3dfaf563b0b879d759648d846cc18f69aa9484
myeonghan-nim <ataraxia9408@gmail.com>
2021-09-30 20:28:52 +0900

    Change: explanatory notes

--- wunderlist/urls.py
+++ wunderlist/urls.py
@@ -1,4 +1,4 @@
-"""wunderlist URL Configuration
+'''wunderlist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -12,13 +12,13 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('accounts/', include('accounts.urls')),
 
     path('', include('todos.urls')),
