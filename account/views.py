3c074c8960cbd37ae50cc70b8d0bcfca5ffc5b14
myeonghan-nim <hannim0828@gmail.com>
2021-09-30 20:40:44 +0900

    Change: app name and account logic

--- account/views.py
+++ account/views.py
@@ -1,26 +1,18 @@
-from django.shortcuts import render, redirect
-
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
 
 def signup(request):
-
     if request.user.is_authenticated:
         return redirect('todos:index')
 
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-
         if form.is_valid():
-
             user = form.save()
             auth_login(request, user)
-
             return redirect(request.GET.get('next') or 'todos:index')
     else:
         form = UserCreationForm()
@@ -29,22 +21,18 @@ def signup(request):
         'form': form,
     }
 
-    return render(request, 'accounts/form.html', context)
+    return render(request, 'account/form.html', context)
 
 
 def login(request):
-
     if request.user.is_authenticated:
         return redirect('todos:index')
 
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
-
         if form.is_valid():
             auth_login(request, form.get_user())
-
             return redirect('todos:index')
-
     else:
         form = AuthenticationForm()
 
@@ -52,11 +40,9 @@ def login(request):
         'form': form,
     }
 
-    return render(request, 'accounts/form.html', context)
+    return render(request, 'account/form.html', context)
 
 
 def logout(request):
-
     auth_logout(request)
-
     return redirect('todos:index')
