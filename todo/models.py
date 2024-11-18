cfd2cfe31bb02b3df896229b222bb80303ef8a86
myeonghan-nim <hannim0828@gmail.com>
2024-11-18 20:55:33 +0900

    Delete: previous applications

--- todo/models.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.db import models
-
-
-class Todo(models.Model):
-    content = models.CharField(max_length=100)
-    due_date = models.DateField()
-    created_at = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
